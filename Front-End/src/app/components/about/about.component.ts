d69c56874a98c81284403291adce7b06b6404186
Scoppietto <matiaschioppa@gmail.com>
2023-05-14 19:42:52 -0300

    point controll

--- Front-End/src/app/components/about/about.component.ts
+++ Front-End/src/app/components/about/about.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { person } from 'src/app/model/person.model';
+import { PersonService } from 'src/app/service/person.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements{
+  person: person = new person("","","");
 
+  constructor(public personService: PersonService) { }
+
+  ngOnInit(): void{
+  this.personService.getPerson().subscribe(data => {this.person = data.person})
+  }
 }
