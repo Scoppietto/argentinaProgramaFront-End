747b4c6d19c7e8c52fa323d469db5653f727c2d4
Scoppietto <matiaschioppa@gmail.com>
2023-05-13 21:25:20 -0300

    header + bootstrap
    +bootstrap
    +header
    +banner
    +logo-ap
    +red-social
    +imgs

---
+++ Front-End/src/app/components/social-red/social-red.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-red',
+  templateUrl: './social-red.component.html',
+  styleUrls: ['./social-red.component.css']
+})
+export class SocialRedComponent {
+
+}
